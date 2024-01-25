053d7d8d0e1091eff73abfe89b71c522a4d49aec
Jay Qi <jayqi@users.noreply.github.com>
2024-01-24 20:15:46 -0500

    Add American Red Cross as author; fix license

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     packages=find_packages(),
     version='0.1.0',
     description='A short description of the project.',
-    author='Your name (or your organization/company/team)',
-    license='MIT',
+    author='The American National Red Cross',
+    license='BSD 3-Clause',
 )
